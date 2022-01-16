702e79ae87b2bcad3ad050103392d35052fd6d08
konpeeyush <piyushksingh2589@gmail.com>
2022-01-16 19:06:04 +0530

    Making some changes.

--- Chapter5.py
+++ Chapter5.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import cv2
+import numpy as np
+img = cv2.imread("Resources/cards.jpeg")
+width, height = 250, 350
+pts1 = np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])
+pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+output = cv2.warpPerspective(img, matrix, (width, height))
+cv2.imshow("Image", img)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
+
